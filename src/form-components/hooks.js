7f1dd4af8d325671d8265d07a3556291f75f776d
Ariel Pettyjohn <me@arielpettyjohn.com>
2024-07-14 14:59:18 -0700

    Remove cruft

--- src/form-components/hooks.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export function useKey() {
-    const [key, setKey] = useState(1);
-    const incrementKey = () => setKey(({ key }) => ({ key: key + 1 }));
-    return { key, incrementKey };
-}
