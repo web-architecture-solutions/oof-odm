1174539cc888884f313bfbfcf16985f567c3a00f
Ariel Pettyjohn <me@arielpettyjohn.com>
2024-07-20 14:05:37 -0700

    Remove cruft

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,9 +1,3 @@
-import styles from "./Loading.module.css";
-
 export default function Loading() {
-    return (
-        <div className={styles.LoadingView}>
-            Loading...
-        </div>
-    );
+    return <div>Loading...</div>;
 }
